352a2d0819b31a7f7af67750f119ef9389aa5c01
KITTIPHON SINGCHOM <67271534+bossk774158@users.noreply.github.com>
2022-11-07 22:42:43 +0700

    Add employee/patient model

--- lib/utils/models/user/employee.dart
+++ lib/utils/models/user/employee.dart
@@ -1,1 +1,28 @@
-class Employee {}
+class Employee {
+  final int employeeID;
+  final String employeeFirstName;
+  final bool employeeIsAdmin;
+  final String employeePhoneNumber;
+  final int employeeRole;
+  final int employeeDepartment;
+  final String employeePassword;
+
+  const Employee({
+    required this.employeeID,
+    required this.employeeFirstName,
+    required this.employeeIsAdmin,
+    required this.employeePhoneNumber,
+    required this.employeeRole,
+    required this.employeeDepartment,
+    required this.employeePassword,
+  });
+
+  static Employee fromJson(json) => Employee(
+      employeeID: json['employeeID'],
+      employeeFirstName: json['employeeFirstName'],
+      employeeIsAdmin: json['employeeIsAdmin'],
+      employeePhoneNumber: json['employeePhoneNumber'],
+      employeeRole: json['employeeRole'],
+      employeeDepartment: json['employeeDepartment'],
+      employeePassword: json['employeePassword']);
+}
